50eceb6e5961859bf6f50473def789ebe85da284
박성현 <tjdeorkfl95@naver.com>
2021-11-24 23:00:06 +0900

    fix the error of housedetail

--- web_ssafy/src/components/house/HouseSearchBar.vue
+++ web_ssafy/src/components/house/HouseSearchBar.vue
@@ -93,6 +93,7 @@ export default {
   created() {
     // this.$store.dispatch("getSido");
     // this.sidoList();
+    this.CLEAR_DETAIL_HOUSE();
     this.CLEAR_SIDO_LIST();
     this.getSido();
   },
@@ -200,6 +201,7 @@ export default {
         params,
         (response) => {
           // console.log(response.data.response.body.items.item);
+          this.CLEAR_DETAIL_HOUSE();
           this.houses = response.data.response.body.items.item;
           this.SET_HOUSE_LIST(this.houses);
           this.listkey *= -1;
