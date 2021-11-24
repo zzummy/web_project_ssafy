3e375c12196537b02b35c4b9fd40c4ca2b49c45e
박성현 <tjdeorkfl95@naver.com>
2021-11-24 20:57:35 +0900

    update board pagination and search

--- web_ssafy/src/components/board/BoardList.vue
+++ web_ssafy/src/components/board/BoardList.vue
@@ -16,6 +16,26 @@
     </b-row>
     <b-row class="mb-1">
       <b-col class="text-right">
+        <b-form-select
+          class="mr-2"
+          v-model="key"
+          size="ms"
+          id="select"
+          :options="options"
+        ></b-form-select>
+        <input
+          type="text"
+          id="word"
+          @keyup.enter="searchArticles()"
+          class="mr-2"
+          v-model="word"
+        />
+        <b-button
+          @click="searchArticles()"
+          class="mr-2"
+          style="border: 0px; background-color: #426289"
+          >검색
+        </b-button>
         <b-button
           @click="moveWrite()"
           style="border: 0px; background-color: #426289"
@@ -26,7 +46,7 @@
     <br />
     <b-row>
       <b-col v-if="articles.length">
-        <b-table-simple hover responsive>
+        <b-table-simple hover responsive id="items">
           <b-thead style="background-color: #f2f4f8">
             <b-tr>
               <b-th>글번호</b-th>
@@ -39,14 +59,29 @@
           <tbody>
             <!-- 하위 component인 ListRow에 데이터 전달(props) -->
             <board-list-row
-              v-for="(article, index) in articles"
+              v-for="(article, index) in slicedarticles"
               :key="index"
               v-bind="article"
             />
           </tbody>
         </b-table-simple>
       </b-col>
-      <!-- <b-col v-else class="text-center">도서 목록이 없습니다.</b-col> -->
+      <b-col v-else class="text-center">게시글(QnA)이 없습니다.</b-col>
+    </b-row>
+    <b-row>
+      <b-col></b-col>
+      <b-col>
+        <!-- Pagination 처리 -->
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="totalrows"
+          :per-page="perPage"
+          aria-controls="items"
+          align="center"
+          @page-click="pageClick"
+        ></b-pagination>
+      </b-col>
+      <b-col></b-col>
     </b-row>
   </b-container>
 </template>
@@ -54,6 +89,7 @@
 <script>
 import BoardListRow from "@/components/board/child/BoardListRow";
 import { listArticle } from "@/api/board.js";
+import http from "@/util/http-common";
 
 export default {
   name: "BoardList",
@@ -62,7 +98,19 @@ export default {
   },
   data() {
     return {
+      // 검색
+      key: null,
+      options: [
+        { value: null, text: "선택" },
+        { value: "subject", text: "제목" },
+        { value: "content", text: "내용" },
+      ],
+      word: "",
       articles: [],
+      slicedarticles: [],
+      perPage: 5,
+      currentPage: 1,
+      totalrows: 0,
     };
   },
   created() {
@@ -76,6 +124,11 @@ export default {
       param,
       (response) => {
         this.articles = response.data;
+        this.totalrows = this.articles.length;
+        this.slicedarticles = this.articles.slice(
+          (this.currentPage - 1) * this.perPage,
+          this.currentPage * this.perPage
+        );
       },
       (error) => {
         console.log(error);
@@ -85,6 +138,42 @@ export default {
   methods: {
     moveWrite() {
       this.$router.push({ name: "BoardWrite" });
+    },
+    // 검색
+    searchArticles() {
+      http.get(`/board?key=${this.key}&word=${this.word}`).then(({ data }) => {
+        console.log(data);
+        let param = {
+          pg: this.pg,
+          spp: this.spp,
+          // pg: 1,
+          // spp: 10,
+          key: this.key,
+          word: this.word,
+        };
+        listArticle(
+          param,
+          (response) => {
+            this.articles = response.data;
+            this.totalrows = this.articles.length;
+            this.currentPage = 1;
+            this.slicedarticles = this.articles.slice(
+              (this.currentPage - 1) * this.perPage,
+              this.currentPage * this.perPage
+            );
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      });
+    },
+    pageClick: function (button, page) {
+      this.currentPage = page;
+      this.slicedarticles = this.articles.slice(
+        (this.currentPage - 1) * this.perPage,
+        this.currentPage * this.perPage
+      );
     },
   },
 };
